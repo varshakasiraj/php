b44faab524d1b17c6a1cd3151da7f78420249a62
varshakasiraj <116131392+varshakasiraj@users.noreply.github.com>
2023-06-19 11:59:06 +0530

    init commit

---
+++ productcart/index.php
@@ -0,0 +1,147 @@
+<?php
+	$url="./assets/css/style.css";
+	echo"<link href='$url'rel='stylesheet' type='text/css'";
+?>
+<html>
+	<head>
+		<title>Productcard</title>
+		<!--<link rel="stylesheet" type="text/css" href="./assets/css/style.css"/>-->
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" 
+		integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw=="
+		crossorigin="anonymous" referrerpolicy="no-referrer" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+
+	</head>
+	<body> 
+		<div class="headline">
+		<h1>Products</h1>
+		<h4>ShowCase your products in this beautiful shop section</h4>
+		</div>
+		<div class="container">
+			<div class="wrapper">
+				<div class="card">
+					<div class="image">
+						<img src="./assets/image/bag1.jpg" alt="bag"/>
+					</div>
+					<div class="title">
+						<h4>Trending</h4>
+					</div>
+					<div class="description">
+						<h3>Handbag Pouch Set</h3>
+						<p>As absoulte is by amounted repeated entierly ye returned
+						These ready timed enjoy might sir yet one sinceAs absoulte is by amounted repeated entierly ye returned
+						These ready timed enjoy might sir yet one sinceAs absoulte is by amounted repeated entierly ye returned
+						These ready timed enjoy might sir yet one since</p>
+					</div>
+					<div class="price">
+						<h3>$82.00 - $99.00</h3>
+						<i class="fa fa-cart-shopping"></i>
+					</div>
+					<div class="buy">
+					<button>Buy Now</button>
+					</div>
+				</div>
+				<div class="card">
+					<div class="image">
+						<img src="./assets/image/highjean.jpg" alt="bag"/>
+					</div>
+					<div class="title">
+						<h4>Fashion</h4>
+					</div>
+					<div class="description">
+						<h3>Waist Straight jean</h3>
+						<p>As absoulte is by amounted repeated entierly ye returned
+						These ready timed enjoy might sir yet one since</p>
+					</div>
+					<div class="price">
+						<h3>$123.00</h3>
+						<i class="fa fa-cart-shopping"></i>
+					</div>
+					<div class="buy">
+						<button>Buy Now</button>
+					</div>
+				</div>
+				<div class="card">
+					<div class="image">
+						<img src="./assets/image/bag2.jpg" alt="bag"/>
+					</div>
+					<div class="title">
+						<h4>Trending</h4>
+					</div>
+					<div class="description">
+						<h3>Butterflies Handbag</h3>
+						<p>As absoulte is by amounted repeated entierly ye returned
+						These ready timed enjoy might sir yet one since</p>
+					</div>
+					<div class="price">
+						<h3>$119.00</h3>
+						<i class="fa fa-cart-shopping"></i>
+					</div>
+					<div class="buy">
+					<button>Buy Now</button>
+					</div>
+				</div>
+				<div class="card">
+					<div class="image">
+						<img src="./assets/image/bag3.jpg" alt="bag"/>
+					</div>
+					<div class="title">
+						<h4>Fashion</h4>
+					</div>
+					<div class="description">
+						<h3>Glory Handbag</h3>
+						<p>As absoulte is by amounted repeated entierly ye returned
+						These ready timed enjoy might sir yet one since</p>
+					</div>
+					<div class="price">
+						<h3>$99.00</h3>
+						<i class="fa fa-cart-shopping"></i>
+					</div>
+					<div class="buy">
+					<button>Buy Now</button>
+					</div>
+				</div>
+				<div class="card">
+					<div class="image">
+						<img src="./assets/image/bag4.jpg" alt="bag"/>
+					</div>
+					<div class="title">
+						<h4>Fashion</h4>
+					</div>
+					<div class="description">
+						<h3>Glory Handbag</h3>
+						<p>As absoulte is by amounted repeated entierly ye returned
+						These ready timed enjoy might sir yet one since</p>
+					</div>
+					<div class="price">
+						<h3>$99.00</h3>
+						<i class="fa fa-cart-shopping"></i>
+					</div>
+					<div class="buy">
+					<button>Buy Now</button>
+					</div>
+				</div>
+				<div class="card">
+					<div class="image">
+						<img src="./assets/image/bag5.jpg" alt="bag"/>
+					</div>
+					<div class="title">
+						<h4>Fashion</h4>
+					</div>
+					<div class="description">
+						<h3>Glory Handbag</h3>
+						<p>As absoulte is by amounted repeated entierly ye returned
+						These ready timed enjoy might sir yet one since</p>
+					</div>
+					<div class="price">
+						<h3>$99.00</h3>
+						<i class="fa fa-cart-shopping"></i>
+					</div>
+					<div class="buy">
+					<button>Buy Now</button>
+					</div>
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
