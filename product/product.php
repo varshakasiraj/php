67c6d48b49b42d56281a988d4f29152a072810ec
varshakasiraj <116131392+varshakasiraj@users.noreply.github.com>
2023-06-26 18:20:06 +0530

    image validation

--- product/product.php
+++ product/product.php
@@ -14,53 +14,68 @@ class product
         global $db;
         $insert_query = $db->insert_query("insert into iteam(img,title,description_tag,description,price)" . "values(
             $insert_input)");
+            
         return $insert_query;
     }
     public function processPostProduct()
     {
         global $_POST;
         global $_FILES;
-
+        global $errors;
         if (!empty($_POST['formaction']) && $_POST['formaction'] == "insertProduct") {
-
-            global $asset_uri;
+            $errors = array();
+            global $asset_uri, $error;
             $error = array(
-                "img" => "check your image filed",
-                "title" => " Title filed Contains  less or more than 8 to 10 character ",
-                "description_tag" => "description tag Contains  less or more than 8 to 10 character ",
+                "img" => "your file is not acceptable or empty",
+                "title" => " Title filed Contains  less or more than 8 to 10 character or empty",
+                "description_tag" => "description tag Contains  less or more than 8 to 10 character  or empty",
                 "price" => "only float value is acceptable"
             );
-            $allowed_img_extension = array("png", "jpg", "jpeg", "jfif");
-            $path = $_FILES['img']['tmp_name'];
-            $basename = basename($_FILES["img"]["name"]);
-            $image_path = $asset_uri . "/image/products/" . $basename;
-            $extension = $ext = substr(strrchr($basename, '.'), 1);
             $title = $_POST['title'];
             $description_tag = $_POST['description_tag'];
             $description = $_POST['description'];
             $price = $_POST['price'];
-            if (!empty([$_FILES["img"]]) && in_array($extension, $allowed_img_extension)) {
-
-                move_uploaded_file($path, $image_path);
-
+            $image = $_FILES['img'];
+            $img = $this->imagevalidation($image);
+            if (empty($title) && !preg_match("/^[a-z]{15,20}/", $description_tag)) {
+                $errors[] = $error["description_tag"];
             }
-            if (!preg_match("/^[A-z]{15,20}/", $description_tag)) {
-                echo $error["title"];
+            if (empty($title) && !preg_match("/^[a-z]{8,10}/", $title)) {
+                $errors[] = $error["title"];
             }
-            if (!preg_match("/^[A-z]{8,10}/", $title)) {
-                echo $error["title"];
+            if (empty($price) && !is_float($price)) {
+                $errors[] = $error["price"];
             }
-            if (!is_float($price)) {
-                echo $error["price"];
-            }
-            // $insert_input="'$img'," . "'$title',"."'$description_tag',"."'$description',"."'$price'";
-            //  return $this->insertProduct($insert_input) ;
+           // $insert_input = "'$img'," . "'$title'," . "'$description_tag'," . "'$description'," . "'$price'";
+            return [
+                //"sucess" => $this->insertProduct($insert_input),
+                "errors" => $errors
+            ];
         }
-
 
     }
 
-    
+    public function imagevalidation($image)
+    {
+        global $_POST;
+        global $asset_uri, $error,$errors,$img_path;
+        $allowed_img_extension = array("png", "jpg", "jpeg", "jfif");
+        $basename = basename($_FILES["img"]["name"]);
+        $extension = substr(strrchr($basename, '.'), 1);
+        if (!empty($image) && in_array($extension, $allowed_img_extension)) {
+            $path = $_FILES['img']['tmp_name'];
+            $image_path = $asset_uri."\image\products\\" . $basename;
+            move_uploaded_file($path, $image_path);
+            $img_path='/php/assets/image/products/'.$basename;
+        } else {
+            $errors[]=$error['img'];
+        }
+        return $img_path;
+
+    }
+
+
+
 }
 $products_obj = new product();
 ?>
